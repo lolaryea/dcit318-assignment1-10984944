bd754e0b2b392682c15eb2c69b5f56eafea741e4
lolaryea <lolaryea001@st.ug.edu.gh>
2024-06-01 21:32:36 +0000

    Add project files.

---
+++ Grade Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GradeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a numerical grade between 0 and 100: ");
+            double grade = Convert.ToDouble(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (grade >= 70 && grade <= 89)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 60 && grade <= 69)
+            {
+                Console.WriteLine("C");
+            }
+            else if (grade >= 50 && grade <= 59)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
